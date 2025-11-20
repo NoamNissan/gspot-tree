a19057af494ca2f256b2d6a15228620f840b11c5
itaitai <super.gnx@gmail.com>
2025-11-20 11:42:29 +0200

    allow scripts to be run out of package
    Change-Id: I09bdcc058ce941bbff5b19eb7afa4c83a2a9b205

--- led_ctrl/start_persistent_gui.py
+++ led_ctrl/start_persistent_gui.py
@@ -3,7 +3,16 @@
 Start persistent GUI server for LED visualization
 """
 import argparse
-from mock_neopixel import start_persistent_gui
+import sys
+import os
+
+# Handle both direct script execution and module import
+if __name__ == "__main__" and __package__ is None:
+    # Add parent directory to path for direct script execution
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    __package__ = "led_ctrl"
+
+from .mock_neopixel import start_persistent_gui
 
 def main():
     parser = argparse.ArgumentParser(description='Start persistent LED GUI')
@@ -18,8 +27,8 @@ def main():
     tree_structure = None
     if args.tree_config:
         try:
-            from tree_config import load_tree_config
-            from pipeline_demo import TreeStructure
+            from .tree_config import load_tree_config
+            from .pipeline_demo import TreeStructure
             
             tree_data = load_tree_config(args.tree_config, verbose=True)
             tree_structure = TreeStructure(tree_data['rings'], tree_data['branches'])
